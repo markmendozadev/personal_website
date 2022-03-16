02e74474f7a1e93239aca85b9eee95b5457585d2
markmendozadev <markmendozadev@gmail.com>
2022-03-16 14:41:50 +0800

    updated projects

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,10 +1,37 @@
-import image from '../../assets/markmendoza.jpg'
+import image from "../../assets/markmendoza.jpg";
+import Button from "../Button";
 const Project = (props) => {
-  return (
-    <div className={`${props.isVisible ? 'projects__anim' : ''}`} style={props.style}>
-      <img src={image} width="100%" height="100%" alt='project' />
-    </div>
-  )
-}
+  const { data } = props;
 
-export default Project
+  return (
+    <div
+      className={`${props.isVisible ? "projects__anim" : ""}`}
+      style={props.style}
+    >
+      <h2
+        className={`${props.isVisible ? "projects__anim" : ""}`}
+        style={{ textAlign: "center", marginBottom: "0.5rem" }}
+      >
+        {data.title}
+      </h2>
+      <img
+        src={data.img}
+        width="100%"
+        height="80%"
+        alt="project"
+        style={{ marginBottom: "0.2rem" }}
+      />
+      <div className="button__container__projects">
+        <Button
+          href={data.url}
+          target="_blank"
+          style={{ textDecoration: "none" }}
+        >
+          View Demo
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,23 +1,71 @@
-import '../../App.css'
-import './projects.css'
-import Project from './Project'
-import useObserver from '../Hooks'
-
+import "../../App.css";
+import "./projects.css";
+import Project from "./Project";
+import useObserver from "../Hooks";
+import simpleTodoImg from "../../assets/simpleTodo.png";
+import personalPortfolio from "../../assets/My_Portfolio_Demo.png";
+import cryptoGuy from "../../assets/cryptoGuy.png";
+const data = [
+  {
+    id: 1,
+    title: "Todo List Web App",
+    img: simpleTodoImg,
+    url: "https://marktodolist.netlify.app/",
+    stack: ["Material UI", "ReactJS"],
+    delay: "300ms",
+  },
+  {
+    id: 2,
+    title: "Personal Portfolio",
+    img: personalPortfolio,
+    url: "https://markmendoza.netlify.app/",
+    stack: ["Material UI", "ReactJS"],
+    delay: "400ms",
+  },
+  {
+    id: 2,
+    title: "Crypto coins information Web App",
+    img: cryptoGuy,
+    url: "https://cryptoguy.netlify.app/",
+    stack: ["ReactJS"],
+    delay: "500ms",
+  },
+];
 const Projects = () => {
-  const [containerRef,isVisible] = useObserver();
-  console.log(isVisible)
+  const [containerRef, isVisible] = useObserver();
+  console.log(isVisible);
   return (
-    <div id='projects' className='projects' ref={containerRef}>
-      <h2 className={`main__text ${isVisible ? 'projects__anim' : ''}`}  style={{transitionDelay: "50ms"}}>PROJECTS</h2>
-      <p className={`sub__text ${isVisible ? 'projects__anim' : ''}`}  style={{transitionDelay: "200ms"}}><a href="https://github.com/markmendozadev?tab=repositories" target='_blank' rel='noreferrer'>view all projects</a></p>
-      <div className='project__grid'>
-        <Project  isVisible={isVisible} style={{transitionDelay: "400ms"}} />
-        <Project  isVisible={isVisible} style={{transitionDelay: "600ms"}} />
-        <Project  isVisible={isVisible} style={{transitionDelay: "800ms"}} />
-        <Project isVisible={isVisible} style={{transitionDelay: "1000ms"}} />
+    <div id="projects" className="projects" ref={containerRef}>
+      <h2
+        className={`main__text ${isVisible ? "projects__anim" : ""}`}
+        style={{ transitionDelay: "50ms" }}
+      >
+        PROJECTS
+      </h2>
+      <p
+        className={`sub__text ${isVisible ? "projects__anim" : ""}`}
+        style={{ transitionDelay: "200ms" }}
+      >
+        <a
+          href="https://github.com/markmendozadev?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          view all projects
+        </a>
+      </p>
+      <div className={`project__grid`}>
+        {data.map((project) => (
+          <Project
+            key={project.id}
+            data={project}
+            isVisible={isVisible}
+            style={{ transitionDelay: project.delay }}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
